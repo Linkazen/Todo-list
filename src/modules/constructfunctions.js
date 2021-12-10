713f4f085ff579d4acecf7f665aeba669534817e
Jonathan <Jpch2k4@gmail.com>
2021-12-10 15:10:25 +0000

    Function for constructing form is done

--- src/modules/constructfunctions.js
+++ src/modules/constructfunctions.js
@@ -11,17 +11,43 @@ function createTodoForm() {
     let inputs = []
     let labels = []
 
+    console.log(names.length)
+
     for(let i = 0; i < names.length; i++) {
         let tempdiv = document.createElement("input")
         tempdiv.name = names[i]
         inputs.push(tempdiv)
     }
-
+    
     for(let i = 0; i < names.length; i++) {
         let tempdiv = document.createElement("label")
         tempdiv.for = names[i]
         labels.push(tempdiv)
     }
 
-    
+    for(let i = 0; i < names.length; i++) {
+        let label = labels[i]
+        let input = inputs[i]
+        let paragraph = document.createElement("p")
+        paragraph.textContent = "when does the to-do need finishing?"
+        
+        if(i == 2) {
+            formshell.appendChild(paragraph)
+            console.log("hello")
+        }
+
+        if(i < 2 || i > 3) {
+            formshell.appendChild(input)
+            formshell.appendChild(label)
+        }
+        formshell.appendChild(label)
+        formshell.appendChild(input)
+    }
+    return formshell
 }
+
+function test() {
+    console.log(createTodoForm())
+}
+
+export { test }
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import { test } from "./modules/constructfunctions";
+
+let todobtn = document.querySelector("#todobtn")
+
+todobtn.addEventListener("click", test)
