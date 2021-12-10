b7f4aa5c8e409026add1c0441ecca5baef4af6d9
Jonathan <Jpch2k4@gmail.com>
2021-12-10 16:24:08 +0000

    Function now woeks with button.

--- src/modules/constructfunctions.js
+++ src/modules/constructfunctions.js
@@ -5,35 +5,57 @@ function todoConstructor(title, desc, datedue, priority) {
 }
 
 function createTodoForm() {
-    let formshell = document.querySelector("form")
+    let formshell = document.createElement("form")
 
-    let names = ["title", "desc", "nodate", "enterdate", "picktime", "important"]
+    let names = [
+        "title", 
+        "desc", 
+        "nodate", 
+        "enterdate", 
+        "picktime", 
+        "important"
+    ]
+
+    let types = [
+        "text",
+        "text",
+        "radio",
+        "radio",
+        "datetime-local",
+        "checkbox"
+    ]
+
+    let textstuff = [
+        "Enter The Name", 
+        "task specifications", 
+        "Whenever", 
+        "Specific date", 
+        "Pick a date and time",
+        "Important?"
+    ]
+
     let inputs = []
     let labels = []
 
-    console.log(names.length)
-
     for(let i = 0; i < names.length; i++) {
-        let tempdiv = document.createElement("input")
-        tempdiv.name = names[i]
-        inputs.push(tempdiv)
-    }
-    
-    for(let i = 0; i < names.length; i++) {
-        let tempdiv = document.createElement("label")
-        tempdiv.for = names[i]
-        labels.push(tempdiv)
+        let tempinputdiv = document.createElement("input")
+        let templabeldiv = document.createElement("label")
+        tempinputdiv.name = names[i]
+        templabeldiv.for = names[i]
+        inputs.push(tempinputdiv)
+        labels.push(templabeldiv)
     }
 
     for(let i = 0; i < names.length; i++) {
         let label = labels[i]
         let input = inputs[i]
         let paragraph = document.createElement("p")
+        label.textContent = textstuff[i]
+        input.type = types[i]
         paragraph.textContent = "when does the to-do need finishing?"
         
         if(i == 2) {
             formshell.appendChild(paragraph)
-            console.log("hello")
         }
 
         if(i < 2 || i > 3) {
@@ -47,7 +69,8 @@ function createTodoForm() {
 }
 
 function test() {
-    console.log(createTodoForm())
+    let mainarea = document.querySelector("#content")
+    mainarea.appendChild(createTodoForm())
 }
 
 export { test }
